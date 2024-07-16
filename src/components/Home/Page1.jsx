f299f7d8ab8b7a02707e6518fd5f362c86cc89dc
Fishniki <nikichum96@gmail.com>
2024-07-16 14:00:20 +0700

    dsh

--- src/components/Home/Page1.jsx
+++ src/components/Home/Page1.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { FaArrowDown, FaArrowUp } from "react-icons/fa"
 
 const Page1 = () => {
 
@@ -8,19 +9,19 @@ const Page1 = () => {
   }
 
   return (
-    <div className="min-h-screen bg-gray-800 text-white">
+    <div className="min-h-screen  text-white">
       <div className="w-full">
         <div className="py-8 mx-2 my-2 rounded-xl border-4 border-gray-700">
-          <h1 className="text-center text-xl sm:text-2xl md:text-3xl font-bold">Selamat Datang di Momon</h1>
+          <h1 className="text-center text-xl sm:text-2xl md:text-3xl text-slate-700 font-bold">Selamat Datang di Momon</h1>
         </div>
 
         <div className="text-center text-xl font-semibold gap-2 flex justify-center py-5">
-          <button className="px-5 py-2 bg-green-500 hover:bg-green-600 rounded-md text-white transition duration-300" onClick={handleKeuangan}>Pemasukan</button>
-          <button className="px-5 py-2 bg-red-500 hover:bg-red-600 rounded-md text-white transition duration-300" onClick={handleKeuangan}>Pengeluaran</button>
+          <button className="px-5 py-2 bg-green-500 hover:bg-green-600 rounded-md flex items-center text-white transition duration-300 gap-3" onClick={handleKeuangan}>Pemasukan <FaArrowDown /></button>
+          <button className="px-5 py-2 bg-red-500 hover:bg-red-600 rounded-md text-white transition duration-300 flex items-center gap-3" onClick={handleKeuangan}>Pengeluaran  <FaArrowUp /></button>
         </div>
 
         {keuangan ? (
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5 px-4 py-2">
+          <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-2 px-4 py-2">
             <div className="w-full h-[80px] sm:h-[100px] lg:h-[160px] bg-red-500 flex flex-col justify-center items-center rounded-lg shadow-md">
               <h1 className="text-xl font-bold">Hari Ini</h1>
               <p className="text-[15px] font-semibold">Rp. 2.000.000</p>
@@ -39,8 +40,8 @@ const Page1 = () => {
             </div>
           </div>
         ) : (
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5 px-4 py-2">
-            <div className="w-full h-[80px] sm:h-[100px] lg:h-[160px] bg-green-500 flex flex-col justify-center items-center rounded-lg shadow-md">
+          <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-2 px-4 py-2">
+            <div className="w-full h-[80px] sm:h-[100px] lg:h-[160px] bg- flex flex-col justify-center items-center rounded-lg shadow-md">
               <h1 className="text-xl font-bold">Hari Ini</h1>
               <p className="text-[15px] font-semibold">Rp. 4.000.000</p>
             </div>
