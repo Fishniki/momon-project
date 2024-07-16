ce5ee85a3e3d447b9272e1189c8cd6896bf9e476
Fishniki <nikichum96@gmail.com>
2024-07-16 14:56:45 +0700

    dashboard

--- src/components/Home/Page1.jsx
+++ src/components/Home/Page1.jsx
@@ -15,45 +15,45 @@ const Page1 = () => {
           <h1 className="text-center text-xl sm:text-2xl md:text-3xl text-slate-700 font-bold">Selamat Datang di Momon</h1>
         </div>
 
-        <div className="text-center text-xl font-semibold gap-2 flex justify-center py-5">
-          <button className="px-5 py-2 bg-green-500 hover:bg-green-600 rounded-md flex items-center text-white transition duration-300 gap-3" onClick={handleKeuangan}>Pemasukan <FaArrowDown /></button>
-          <button className="px-5 py-2 bg-red-500 hover:bg-red-600 rounded-md text-white transition duration-300 flex items-center gap-3" onClick={handleKeuangan}>Pengeluaran  <FaArrowUp /></button>
+        <div className="text-center text-xl font-semibold gap-2 flex justify-start px-4 py-2">
+          <button className={`px-5 py-2 ${keuangan ? 'bg-red-500 hover:bg-red-600' : 'bg-green-500 hover:bg-green-600'}   rounded-md flex items-center text-white transition duration-300 gap-3`} onClick={handleKeuangan}>{keuangan ? "Pengeluaran" : "Pemasukan"} {keuangan ? <FaArrowUp /> : <FaArrowDown />}</button>
+          
         </div>
 
         {keuangan ? (
           <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-2 px-4 py-2">
-            <div className="w-full h-[80px] sm:h-[100px] lg:h-[160px] bg-red-500 flex flex-col justify-center items-center rounded-lg shadow-md">
+            <div className="w-full h-[100PX] sm:h-[100px] lg:h-[160px] bg-[#FF7777] flex flex-col justify-center items-center rounded-lg shadow-md">
               <h1 className="text-xl font-bold">Hari Ini</h1>
               <p className="text-[15px] font-semibold">Rp. 2.000.000</p>
             </div>
-            <div className="w-full h-[80px] sm:h-[100px] lg:h-[160px] bg-red-500 flex flex-col justify-center items-center rounded-lg shadow-md">
+            <div className="w-full h-[100PX] sm:h-[100px] lg:h-[160px] bg-[#FF7777]  flex flex-col justify-center items-center rounded-lg shadow-md">
               <h1 className="text-xl font-bold">Bulan Ini</h1>
               <p className="text-[15px] font-semibold">Rp. 2.000.000</p>
             </div>
-            <div className="w-full h-[80px] sm:h-[100px] lg:h-[160px] bg-red-500 flex flex-col justify-center items-center rounded-lg shadow-md">
+            <div className="w-full h-[100PX] sm:h-[100px] lg:h-[160px] bg-[#FF7777]  flex flex-col justify-center items-center rounded-lg shadow-md">
               <h1 className="text-xl font-bold">Tahun Ini</h1>
               <p className="text-[15px] font-semibold">Rp. 2.000.000</p>
             </div>
-            <div className="w-full h-[80px] sm:h-[100px] lg:h-[160px] bg-red-500 flex flex-col justify-center items-center rounded-lg shadow-md">
+            <div className="w-full h-[100PX] sm:h-[100px] lg:h-[160px] bg-[#FF7777] flex flex-col justify-center items-center rounded-lg shadow-md">
               <h1 className="text-xl font-bold">Total </h1>
               <p className="text-[15px] font-semibold">Rp. 2.000.000</p>
             </div>
           </div>
         ) : (
           <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 gap-2 px-4 py-2">
-            <div className="w-full h-[80px] sm:h-[100px] lg:h-[160px] bg- flex flex-col justify-center items-center rounded-lg shadow-md">
+            <div className="w-full h-[80px] sm:h-[100px] lg:h-[160px] bg-[#37B7C3] flex flex-col justify-center items-center rounded-lg shadow-md">
               <h1 className="text-xl font-bold">Hari Ini</h1>
               <p className="text-[15px] font-semibold">Rp. 4.000.000</p>
             </div>
-            <div className="w-full h-[80px] sm:h-[100px] lg:h-[160px] bg-green-500 flex flex-col justify-center items-center rounded-lg shadow-md">
+            <div className="w-full h-[80px] sm:h-[100px] lg:h-[160px] bg-[#37B7C3] flex flex-col justify-center items-center rounded-lg shadow-md">
               <h1 className="text-xl font-bold">Bulan Ini</h1>
               <p className="text-[15px] font-semibold">Rp. 4.000.000</p>
             </div>
-            <div className="w-full h-[80px] sm:h-[100px] lg:h-[160px] bg-green-500 flex flex-col justify-center items-center rounded-lg shadow-md">
+            <div className="w-full h-[80px] sm:h-[100px] lg:h-[160px] bg-[#37B7C3] flex flex-col justify-center items-center rounded-lg shadow-md">
               <h1 className="text-xl font-bold">Tahun Ini</h1>
               <p className="text-[15px] font-semibold">Rp. 4.000.000</p>
             </div>
-            <div className="w-full h-[80px] sm:h-[100px] lg:h-[160px] bg-green-500 flex flex-col justify-center items-center rounded-lg shadow-md">
+            <div className="w-full h-[80px] sm:h-[100px] lg:h-[160px] bg-[#37B7C3] flex flex-col justify-center items-center rounded-lg shadow-md">
               <h1 className="text-xl font-bold">Total </h1>
               <p className="text-[15px] font-semibold">Rp. 4.000.000</p>
             </div>
